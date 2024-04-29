4dde7ce296c878ef346dc6fffe65de4811d728c7
sam muriithi <samuelmuriithi965@gmail.com>
2024-04-29 12:51:23 +0300

    project complete

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -50,7 +50,7 @@ export default function Generator(props) {
     }
 
     return (
-        <SectionWrapper id={'generate'} header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
+        <SectionWrapper id={'generate'} header={"shape your session"} title={['It\'s', 'Beast Mode', 'Hour']}>
             <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to endure."} />
             <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
                 {Object.keys(WORKOUTS).map((type, typeIndex) => {
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -13,4 +13,4 @@ export default function SectionWrapper(props) {
             </div>
         </section>
     )
-} 
+}   
